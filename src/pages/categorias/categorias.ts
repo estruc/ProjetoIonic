ce4da153fb51bed15685442f7d1e45dedd18f3e0
Marcelo Estruc <marcelo.estruc@uniriotec.br>
2018-07-27 19:46:43 -0300

    Listando Categorias no console

---
+++ src/pages/categorias/categorias.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../services/domain/categoria.service';
+
+
+
+@IonicPage()
+@Component({
+  selector: 'page-categorias',
+  templateUrl: 'categorias.html',
+})
+export class CategoriasPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService: CategoriaService) {
+  }
+
+  ionViewDidLoad() { // quando a pagina for carregada
+    this.categoriaService.findAll()
+    .subscribe(response => {
+      console.log(response);
+    },
+    error => {
+      console.log(error);
+    });
+  }
+
+
+  // trabalharRetorno(response){
+  //   console.log(response);
+  // }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 
 @IonicPage()
@@ -9,8 +10,22 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
+
+  login(){
+    this.navCtrl.setRoot('CategoriasPage') // vai ficar como página corrente
+  }
+
+  ionViewWillEnter() { //quando entrar na pagina desabilito o menu
+    this.menu.swipeEnable(false);
+  }
+    
+  ionViewDidLeave() { //quando sair da pagina habilito o menu
+    this.menu.swipeEnable(true);
+  }
+
+
 
 }
